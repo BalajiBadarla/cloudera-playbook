4bb6dbfb140eb36793a4a0bb4da07b04da9e1b04
BalajiBadarla <ibalaji9@gmail.com>
2020-01-10 08:42:58 -0800

    bootstrap.sh changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,15 @@ chmod 600 authorized_keys
 chown -R badman:badman /home/badman
 yum install ansible git -y
 sudo su -l badman -c 'git clone https://github.com/BalajiBadarla/cloudera-playbook.git'
-sudo su -l badman -c 'ansible-galaxy install -r cloudera-playbook/requirements.yml'
 sudo su -l badman -c 'sed -i "s#jira-ps7#`hostname -s`#g" cloudera-playbook/hosts'
 sed -i "s/^#host_key_checking/host_key_checking/" /etc/ansible/ansible.cfg
-sudo su -l badman -c 'ansible-playbook -i cloudera-playbook/hosts cloudera-playbook/install-postgres-scl-rh.yml'
-usermod -G wheel postgres
-sudo su -l badman -c "ansible db_server -i cloudera-playbook/hosts -m file -a 'src=/opt/rh/rh-postgresql96/root/usr/lib64/libpq.so.rh-postgresql96-5  dest=/usr/lib64/libpq.so.rh-postgresql96-5 state=link' -b"
-sudo su -l badman -c "ansible db_server -i cloudera-playbook/hosts -m file -a 'src=/opt/rh/rh-postgresql96/root/usr/lib64/libpq.so.rh-postgresql96-5  dest=/usr/lib/libpq.so.rh-postgresql96-5 state=link' -b"
+sudo su -l badman -c 'cd cloudera-playbook; git submodule update --init'
+
+
+# then run the below commands
+sudo su -l badman -c 'ansible-playbook -i cloudera-playbook/hosts cloudera-playbook/install-postgres-scl-rh.yml -e postgresql_version="10.5" -e postgresql_scl_version="10"'
 sudo su -l badman -c 'ansible-playbook -i cloudera-playbook/hosts cloudera-playbook/install-postgres-dbs.yml'
+#usermod -G wheel postgres
+#sudo su -l badman -c 'ansible-galaxy install -r cloudera-playbook/requirements.yml'
+#sudo su -l badman -c "ansible db_server -i cloudera-playbook/hosts -m file -a 'src=/opt/rh/rh-postgresql96/root/usr/lib64/libpq.so.rh-postgresql96-5  dest=/usr/lib64/libpq.so.rh-postgresql96-5 state=link' -b"
+#sudo su -l badman -c "ansible db_server -i cloudera-playbook/hosts -m file -a 'src=/opt/rh/rh-postgresql96/root/usr/lib64/libpq.so.rh-postgresql96-5  dest=/usr/lib/libpq.so.rh-postgresql96-5 state=link' -b"
